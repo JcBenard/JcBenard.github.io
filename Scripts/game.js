1a3a359fa6424888e9f1d0d73f55c63458067c9e
JcBenard <niteburn@hotmail.com>
2015-01-30 01:22:25 +0900

    Created prooduction level

---
+++ Scripts/game.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="lib/impress.js" />
+
+function init() {
+    console.log("Game Started");
+    impress().init();
+
+    for (i = 1; i <= 38; i++) {
+        document.getElementById(i).style.backgroundImage = "url('../assets/images/" + i + ".png')";
+    }
+}
+
+var sub;
+function subweapon(sub) {
+    this.sub = sub;
+    document.getElementsByClassName(slide).style.backgroundImage = "url('../assets/images/sub.png')";
+}
+
+function subCheck(subNeeded, goodOutcome,badOutcome, bossName) {
+    if (sub == subNeeded) {
+        document.getElementById(bossName).href = "#/" + goodOutcome;
+    } else {
+        document.getElementById(bossName).href = "#/" + badOutcome;
+    }
+}
