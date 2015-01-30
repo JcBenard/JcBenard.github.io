3c1e23b54c235339f0302417f1333b4f6f2af4ab
JcBenard <niteburn@hotmail.com>
2015-01-31 07:51:03 +0900

    Update with final game

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,20 +1,29 @@
 ﻿/// <reference path="lib/impress.js" />
+var sub;//Stores the players current subweapon
 
-function init() {
+function init() {//run on page loading
     console.log("Game Started");
     impress().init();
 
+    //set up the background images for the slides
     for (i = 1; i <= 38; i++) {
         document.getElementById(i).style.backgroundImage = "url('../assets/images/" + i + ".png')";
     }
 }
 
-var sub;
+//change the subwepon when they get a new one
 function subweapon(sub) {
     this.sub = sub;
-    document.getElementsByClassName(slide).style.backgroundImage = "url('../assets/images/sub.png')";
+
+    //change all the subweapon images on the slides
+    var subImgs = document.getElementsByTagName("img")
+
+    for (x = 0; x <= subImgs.length; x++) {
+        subImgs[x].src = "/assets/images/" + this.sub + ".png"
+    }
 }
 
+//see if the player has a required subweapon
 function subCheck(subNeeded, goodOutcome,badOutcome, bossName) {
     if (sub == subNeeded) {
         document.getElementById(bossName).href = "#/" + goodOutcome;
